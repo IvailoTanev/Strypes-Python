9917be2fcf9eae61bea3e1c7c827875fcacee300
IvailoTanev <Ivailodt@gmail.com>
2023-07-05 12:42:36 +0300

    initial commit

--- Homework1.py
+++ Homework1.py
@@ -3,20 +3,13 @@
 
 # 1. Sum of all digits of a number
 def sum_of_digits(num):
-    num_of_digits = 0
-    new_num = num
-    
-    for iter in range(0, num):
-        num = num // 10
-        num_of_digits = num_of_digits + num % 10
 
     for iter in range(0, 5):
-        result = result + new_num % 10
-        new_num = new_num // 10
+        result = result + num % 10
+        num = num // 10
    
-
     return result
-print(sum_of_digits(12000))
+print(sum_of_digits(1200))
 
 
 #2. Turn a number into a list of digits
